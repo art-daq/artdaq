9aae79a15ba5eae503ce6f55e44dd04ea4b158bc
Eric Flumerfelt <eflumerf@fnal.gov>
2016-03-04 12:29:04 -0600

    Added in load_factor and pass_factor. load_factor specifies the mix between sleep calls and counter increments, while pass factor allows the filter to randomly select some events to drop from the data stream.
    Also added a FHiCL document describing all of the configuration parameters and their default values.

--- artdaq/ArtModules/RandomDelayFilter_module.cc
+++ artdaq/ArtModules/RandomDelayFilter_module.cc
@@ -48,11 +48,15 @@ private:
   double mean_ms_;
   double sigma_ms_;
 
+  int pass_factor_;
+  double load_factor_;
+
   // Random Engine Setup
   bool isNormal_;
-    std::mt19937 engine_;
-    std::unique_ptr<std::uniform_real_distribution<double>> uniform_distn_;
-    std::unique_ptr<std::normal_distribution<double>> normal_distn_;
+  std::mt19937 engine_;
+  std::unique_ptr<std::uniform_real_distribution<double>> uniform_distn_;
+  std::unique_ptr<std::normal_distribution<double>> normal_distn_;
+  std::unique_ptr<std::uniform_int_distribution<int>> pass_distn_;
 
 };
 
@@ -62,10 +66,13 @@ artdaq::RandomDelayFilter::RandomDelayFilter(fhicl::ParameterSet const & p)
   , max_ms_(p.get<double>("maximum_delay_ms",1000))
   , mean_ms_(p.get<double>("mean_delay_ms", 500))
   , sigma_ms_(p.get<double>("sigma_delay_ms",100))
+  , pass_factor_(p.get<int>("pass_filter_percentage", 100))
+  , load_factor_(p.get<double>("cpu_load_ratio", 1.0))
   , isNormal_(p.get<bool>("use_normal_distribution", false))
   , engine_(p.get<int64_t>("random_seed",271828))
   , uniform_distn_(new std::uniform_real_distribution<double>(min_ms_, max_ms_))
   , normal_distn_(new std::normal_distribution<double>(mean_ms_, sigma_ms_))
+  , pass_distn_(new std::uniform_int_distribution<int>(0,100))
 {
 }
 
@@ -74,24 +81,29 @@ bool artdaq::RandomDelayFilter::filter(art::Event & e)
   double delay = isNormal_ ? (*normal_distn_)(engine_) : (*uniform_distn_)(engine_);
   mf::LogDebug("RandomDelayFilter") << "Simulating processing of event " << e.event() << " by delaying " << std::to_string(delay) << "ms.";
 
+  usleep(1000 * (1 - load_factor_) * delay);
+
   auto i = 0;
   auto now = std::chrono::high_resolution_clock::now();
-  while(std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - now).count() < delay)
+  while(std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - now).count() < delay * load_factor_)
 	{
 	  i = i + 1 % std::numeric_limits<int>::max();
 	}
 
-  return true;
+  return (*pass_distn_)(engine_) < pass_factor_;
 }
 
 void artdaq::RandomDelayFilter::reconfigure(fhicl::ParameterSet const & p)
 {
-  min_ms_ = p.get<double>("minimum_delay_ms",0);
-  max_ms_=p.get<double>("maximum_delay_ms",1000);
-  mean_ms_=p.get<double>("mean_delay_ms", 500);
-  sigma_ms_=p.get<double>("sigma_delay_ms",100);
-  isNormal_=p.get<bool>("use_normal_distribution", false);
-  engine_ = std::mt19937(p.get<int64_t>("random_seed",271828));
+  min_ms_      = p.get<double>("minimum_delay_ms",0);
+  max_ms_      = p.get<double>("maximum_delay_ms",1000);
+  mean_ms_     = p.get<double>("mean_delay_ms", 500);
+  sigma_ms_    = p.get<double>("sigma_delay_ms",100);
+  pass_factor_ = p.get<int>("pass_filter_percentage", 100);
+  load_factor_ = p.get<double>("cpu_load_ratio", 1.0);
+  isNormal_    = p.get<bool>("use_normal_distribution", false);
+  engine_      = std::mt19937(p.get<int64_t>("random_seed",271828));
+
   uniform_distn_.reset(new std::uniform_real_distribution<double>(min_ms_, max_ms_));
   normal_distn_.reset(new std::normal_distribution<double>(mean_ms_, sigma_ms_));
 }
