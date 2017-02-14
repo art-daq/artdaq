70b78f1275086e3c2c8fd79957172577c10e5638
Eric Flumerfelt <eflumerf@fnal.gov>
2017-02-14 13:46:49 -0600

    Adding "NullTransfer" which does nothing, but does it well, as it succeeds every time.

---
+++ artdaq/TransferPlugins/Null_transfer.cc
@@ -0,0 +1,31 @@
+#include "artdaq/TransferPlugins/TransferInterface.hh"
+
+namespace artdaq {
+
+  class NullTransfer : public TransferInterface {
+  public:
+    NullTransfer(const fhicl::ParameterSet&, Role);
+    ~NullTransfer() = default;
+
+    virtual int receiveFragment(artdaq::Fragment& ,
+				size_t ) 
+    { return source_rank(); }
+
+    virtual CopyStatus copyFragment(artdaq::Fragment& ,
+				    size_t = std::numeric_limits<size_t>::max())
+    { return CopyStatus::kSuccess; }
+    virtual CopyStatus moveFragment(artdaq::Fragment&& ,
+				    size_t  = std::numeric_limits<size_t>::max())
+    { return CopyStatus::kSuccess; }
+  private:
+    std::unique_ptr<TransferInterface> theTransfer_;
+  };
+
+}
+
+artdaq::NullTransfer::NullTransfer(const fhicl::ParameterSet& pset, Role role)
+  : TransferInterface(pset, role)
+{
+}
+
+DEFINE_ARTDAQ_TRANSFER(artdaq::NullTransfer)
