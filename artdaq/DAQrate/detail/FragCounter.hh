3f4de1f43145ab06988851a77e5aaa71d5c867a7
Eric Flumerfelt <eflumerf@fnal.gov>
2017-02-23 10:25:59 -0600

    Remove infinite loop for connecting gdb to transfer_driver_mpi. Improve thread safety by making sure std::maps are locked when counting them.

--- artdaq/DAQrate/detail/FragCounter.hh
+++ artdaq/DAQrate/detail/FragCounter.hh
@@ -4,35 +4,37 @@
 #include <unordered_map>
 #include <atomic>
 #include <limits>
+#include <mutex>
 
 namespace artdaq {
-  namespace detail {
-    class FragCounter;
-  }
+	namespace detail {
+		class FragCounter;
+	}
 }
 
 class artdaq::detail::FragCounter {
 public:
-  explicit FragCounter();
-  void incSlot(size_t slot);
-  void incSlot(size_t slot, size_t inc);
-  void setSlot(size_t slot, size_t val);
+	explicit FragCounter();
+	void incSlot(size_t slot);
+	void incSlot(size_t slot, size_t inc);
+	void setSlot(size_t slot, size_t val);
 
-  size_t nSlots() const;
-  size_t count() const;
-  size_t slotCount(size_t slot) const;
-  size_t minCount() const;
+	size_t nSlots() const;
+	size_t count() const;
+	size_t slotCount(size_t slot) const;
+	size_t minCount() const;
 
-  size_t operator[](size_t slot) const { return slotCount(slot); }
+	size_t operator[](size_t slot) const { return slotCount(slot); }
 
 private:
-  std::unordered_map<size_t, std::atomic<size_t>> receipts_;
+	mutable std::mutex receipts_mutex_;
+	std::unordered_map<size_t, std::atomic<size_t>> receipts_;
 };
 
 inline
 artdaq::detail::FragCounter::
 FragCounter()
-  : receipts_()
+	: receipts_()
 {
 }
 
@@ -41,7 +43,7 @@ void
 artdaq::detail::FragCounter::
 incSlot(size_t slot)
 {
-  incSlot(slot, 1);
+	incSlot(slot, 1);
 }
 
 inline
@@ -49,7 +51,8 @@ void
 artdaq::detail::FragCounter::
 incSlot(size_t slot, size_t inc)
 {
-  receipts_[slot].fetch_add(inc);
+	std::unique_lock<std::mutex> lk(receipts_mutex_);
+	receipts_[slot].fetch_add(inc);
 }
 
 inline
@@ -57,7 +60,8 @@ void
 artdaq::detail::FragCounter::
 setSlot(size_t slot, size_t val)
 {
-  receipts_[slot] = val;
+	std::unique_lock<std::mutex> lk(receipts_mutex_);
+	receipts_[slot] = val;
 }
 
 inline
@@ -65,7 +69,8 @@ size_t
 artdaq::detail::FragCounter::
 nSlots() const
 {
-  return receipts_.size();
+	std::unique_lock<std::mutex> lk(receipts_mutex_);
+	return receipts_.size();
 }
 
 inline
@@ -73,11 +78,12 @@ size_t
 artdaq::detail::FragCounter::
 count() const
 {
-  size_t acc = 0;
-  for(auto& it : receipts_) {
-	acc+=it.second;
-  }
-  return acc;
+	std::unique_lock<std::mutex> lk(receipts_mutex_);
+	size_t acc = 0;
+	for (auto& it : receipts_) {
+		acc += it.second;
+	}
+	return acc;
 }
 
 inline
@@ -85,7 +91,8 @@ size_t
 artdaq::detail::FragCounter::
 slotCount(size_t slot) const
 {
-  return receipts_.count(slot) ? receipts_.at(slot).load() : 0;
+	std::unique_lock<std::mutex> lk(receipts_mutex_);
+	return receipts_.count(slot) ? receipts_.at(slot).load() : 0;
 }
 
 inline
@@ -93,11 +100,12 @@ size_t
 artdaq::detail::FragCounter::
 minCount() const
 {
-  size_t min = std::numeric_limits<size_t>::max();
-  for(auto& it : receipts_) {
-	if(it.second < min) min = it.second;
-  }
-  return min;
+	std::unique_lock<std::mutex> lk(receipts_mutex_);
+	size_t min = std::numeric_limits<size_t>::max();
+	for (auto& it : receipts_) {
+		if (it.second < min) min = it.second;
+	}
+	return min;
 }
 
 #endif /* artdaq_DAQrate_detail_FragCounter_hh */
