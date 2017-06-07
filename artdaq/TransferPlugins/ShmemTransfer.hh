79572e525c7ccb68d406d0584c0eeb22a357c8d7
Eric Flumerfelt <eflumerf@fnal.gov>
2017-06-07 11:29:45 -0500

    Use SharedMemoryManager for ShmemTransfer.

--- artdaq/TransferPlugins/ShmemTransfer.hh
+++ artdaq/TransferPlugins/ShmemTransfer.hh
@@ -2,14 +2,9 @@
 #define artdaq_TransferPlugins_ShemmTransfer_hh
 
 #include "fhiclcpp/fwd.h"
-#include <atomic>
 
 #include "artdaq/TransferPlugins/TransferInterface.hh"
-
-#define BUFFER_EMPTY 0UL
-#define WRITING_FRAGMENT 1UL
-#define FRAGMENT_READY 2UL
-#define READING_FRAGMENT 3UL
+#include "artdaq-core/Core/SharedMemoryFragmentManager.hh"
 
 namespace artdaq
 {
@@ -68,33 +63,9 @@ namespace artdaq
 	private:
 		CopyStatus sendFragment(Fragment&& fragment,
 		                        size_t send_timeout_usec, bool reliable = false);
-
-		bool readyForRead_();
-
-		bool readyForWrite_();
-
-		RawDataType* offsetToPtr(size_t offset);
-
-		struct ShmBuffer
-		{
-			size_t offset;
-			size_t fragmentSizeWords;
-			std::atomic<unsigned int> sem;
-			unsigned int writeCount;
-		};
-
-		struct ShmStruct
-		{
-			std::atomic<unsigned int> read_pos;
-			std::atomic<unsigned int> write_pos;
-			ShmBuffer buffers[100];
-		};
-
-
+		
 		size_t send_timeout_usec_;
-		int shm_segment_id_;
-		ShmStruct* shm_ptr_;
-		int shm_key_;
+		std::unique_ptr<SharedMemoryFragmentManager> shm_manager_;
 
 		Role role_;
 	};
