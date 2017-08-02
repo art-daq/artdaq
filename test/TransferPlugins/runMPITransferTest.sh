218adb4966672de12bc19eaa903c15a177271b36
Eric Flumerfelt <eflumerf@fnal.gov>
2017-08-02 14:25:09 -0500

    Revert "More improvements to runMPITransferTest.sh"
    This reverts commit bfbb621e5d989a5ef356efc2d5007d260c3bcaf5.

--- test/TransferPlugins/runMPITransferTest.sh
+++ test/TransferPlugins/runMPITransferTest.sh
@@ -4,10 +4,7 @@ fclfile=$1
 procs=$2
 
 hydra_nameserver &
-ns_pid=$!
-
-mpirun -np $procs -nameserver localhost transfer_driver_mpi $fclfile
-ret=$?
-kill $ns_pid
+ret=`mpirun -np $procs -nameserver localhost transfer_driver_mpi $fclfile`
+killall hydra_nameserver
 
 exit $ret
