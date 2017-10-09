03c64a46465212c49ceed61c877d6c50b8ce3f11
Eric Flumerfelt <eflumerf@fnal.gov>
2017-10-09 13:30:14 -0500

    LoadParameterSet (and therefore, most artdaq applications) now accepts configuration in the form of (in preference order): 1. If -c is - or --, standard input 2. A FHiCL-formatted string 3. A file containing FHiCL

--- artdaq/Application/LoadParameterSet.hh
+++ artdaq/Application/LoadParameterSet.hh
@@ -6,44 +6,81 @@
 #include <iostream>
 namespace bpo = boost::program_options;
 
-inline fhicl::ParameterSet LoadParameterSet(int argc, char* argv[]) {
+inline fhicl::ParameterSet LoadParameterSet(int argc, char* argv[])
+{
 	std::ostringstream descstr;
 	descstr << argv[0]
-		<< " <-c <config-file>> <other-options> [<source-file>]+";
+		<< " <-c <config>> <other-options> [<source-file>]+";
 	bpo::options_description desc(descstr.str());
 	desc.add_options()
-		("config,c", bpo::value<std::string>(), "Configuration file.")
+		("config,c", bpo::value<std::string>(), "Configuration")
 		("help,h", "produce help message");
 	bpo::variables_map vm;
-	try {
+	try
+	{
 		bpo::store(bpo::command_line_parser(argc, argv).options(desc).run(), vm);
 		bpo::notify(vm);
 	}
-	catch (bpo::error const & e) {
+	catch (bpo::error const & e)
+	{
 		std::cerr << "Exception from command line processing in " << argv[0]
 			<< ": " << e.what() << "\n";
 		exit(-1);
 	}
-	if (vm.count("help")) {
+	if (vm.count("help"))
+	{
 		std::cout << desc << std::endl;
 		exit(1);
 	}
-	if (!vm.count("config")) {
+
+
+	fhicl::ParameterSet pset;
+	
+	if (vm.count("config"))
+	{
+		std::string config = vm["config"].as<std::string>();
+
+		if (config == "-" || config == "--")
+		{
+			std::cerr << "Reading configuration from standard input. Press Ctrl-D to end" << std::endl;
+			std::stringstream ss;
+			std::string line;
+			while (std::getline(std::cin, line))
+			{
+				ss << line << std::endl;
+			}
+			std::cin.clear();
+
+			make_ParameterSet(ss.str(), pset);
+		}
+		else
+		{
+			try
+			{
+				make_ParameterSet(config, pset);
+			}
+			catch (fhicl::exception e)
+			{
+				if (getenv("FHICL_FILE_PATH") == nullptr)
+				{
+					std::cerr
+						<< "INFO: environment variable FHICL_FILE_PATH was not set. Using \".\"\n";
+					setenv("FHICL_FILE_PATH", ".", 0);
+				}
+				cet::filepath_lookup_after1 lookup_policy("FHICL_FILE_PATH");
+				make_ParameterSet(vm["config"].as<std::string>(), lookup_policy, pset);
+			}
+		}
+	}
+	else
+	{
 		std::cerr << "Exception from command line processing in " << argv[0]
-			<< ": no configuration file given.\n"
+			<< ": no configuration given.\n"
 			<< "For usage and an options list, please do '"
 			<< argv[0] << " --help"
 			<< "'.\n";
 		exit(2);
 	}
-	fhicl::ParameterSet pset;
-	if (getenv("FHICL_FILE_PATH") == nullptr) {
-		std::cerr
-			<< "INFO: environment variable FHICL_FILE_PATH was not set. Using \".\"\n";
-		setenv("FHICL_FILE_PATH", ".", 0);
-	}
-	cet::filepath_lookup_after1 lookup_policy("FHICL_FILE_PATH");
-	make_ParameterSet(vm["config"].as<std::string>(), lookup_policy, pset);
 	return pset;
 }
 #endif //artdaq_proto_LoadParameterSet_hh
