c68424d1772bde9039aac7a461054ba1b77fcc32
Kurt Biery <biery@fnal.gov>
2018-05-23 12:37:58 -0500

    Added a partition_number_ data member to Globals and started using it in GetPartitionNumber. This is in support of specifying the partition number as a command-line argument to artdaq processes.

--- artdaq/DAQdata/Globals.cc
+++ artdaq/DAQdata/Globals.cc
@@ -3,6 +3,7 @@
 int artdaq::Globals::my_rank_ = -1;
 artdaq::MetricManager* artdaq::Globals::metricMan_ = nullptr;
 std::string artdaq::Globals::app_name_ = "";
+int artdaq::Globals::partition_number_ = -1;
 
 std::string artdaq::Globals::mftrace_iteration_ = "Booted";
 std::string artdaq::Globals::mftrace_module_ = "DAQ";
