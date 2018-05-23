8a06a1f2989a1c18a67251be15efe07f8a5be098
Kurt Biery <biery@fnal.gov>
2018-05-23 15:01:09 -0500

    Pulled in the changes from the feature/cmdline_partno branch so that we can specify the partition number on the artdaq process command line.

--- artdaq/DAQrate/SharedMemoryEventManager.cc
+++ artdaq/DAQrate/SharedMemoryEventManager.cc
@@ -263,10 +263,24 @@ void artdaq::SharedMemoryEventManager::RunArt(std::shared_ptr<art_config_file> c
 
 		std::vector<char*> args{ (char*)"art", (char*)"-c", filename, NULL };
 
-		
 		auto pid = fork();
 		if (pid == 0)
 		{ /* child */
+			// 23-May-2018, KAB: added the setting of the partition number env var
+			// in the environment of the child art process so that Globals.hh
+			// will pick it up there and provide it to the artdaq classes that
+			// are used in data transfers, etc. within the art process.
+			std::string envVarKey = "ARTDAQ_PARTITION_NUMBER";
+			std::string envVarValue = std::to_string(Globals::GetPartitionNumber());
+			if (setenv(envVarKey.c_str(), envVarValue.c_str(), 1) != 0)
+			{
+				TLOG(TLVL_ERROR) << "Error setting environment variable \"" << envVarKey
+				                 << "\" in the environment of a child art process. "
+				                 << "This may result in incorrect TCP port number "
+				                 << "assignments or other issues, and data may "
+				                 << "not flow through the system correctly.";
+			}
+
 			execvp("art", &args[0]);
 			delete[] filename;
 			exit(1);
