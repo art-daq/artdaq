9a9fa495f29f30bea7fc2850c4cdb13c4e2eada1
Eric Flumerfelt <eflumerf@fnal.gov>
2018-06-29 19:38:23 -0500

    Implemented all send_* commands in XMLRPC_commander. Added commander_test which can be used to exercise a Commander plugin.

--- artdaq/ExternalComms/xmlrpc_commander.hh
+++ artdaq/ExternalComms/xmlrpc_commander.hh
@@ -48,6 +48,141 @@ public:
 	/// <param name="monitor_label">Label of the monitor to unregister</param>
 	/// <returns>Return status from XMLRPC</returns>
 	std::string send_unregister_monitor(std::string monitor_label) override;
+	
+	/// <summary>
+	/// Send an init command over XMLRPC
+	/// 
+	/// The init command is accepted by all artdaq processes that are in the booted state.
+	/// It expects a ParameterSet for configuration, a timeout, and a timestamp.
+	/// </summary>
+	/// <returns>Command result: "SUCCESS" if succeeded</returns>
+	std::string send_init(fhicl::ParameterSet, uint64_t, uint64_t);
+
+	/// <summary>
+	/// Send a soft_init command over XMLRPC
+	/// 
+	/// The soft_init command is accepted by all artdaq processes that are in the booted state.
+	/// It expects a ParameterSet for configuration, a timeout, and a timestamp.
+	/// </summary>
+	/// <returns>Command result: "SUCCESS" if succeeded</returns>
+	std::string send_soft_init(fhicl::ParameterSet, uint64_t, uint64_t);
+
+	/// <summary>
+	/// Send a reinit command over XMLRPC
+	/// 
+	/// The reinit command is accepted by all artdaq processes.
+	/// It expects a ParameterSet for configuration, a timeout, and a timestamp.
+	/// </summary>
+	/// <returns>Command result: "SUCCESS" if succeeded</returns>
+	std::string send_reinit(fhicl::ParameterSet, uint64_t, uint64_t);
+
+	/// <summary>
+	/// Send a start command over XMLRPC
+	/// 
+	/// The start command starts a Run using the given run number.
+	/// This command also accepts a timeout parameter and a timestamp parameter.
+	/// </summary>
+	/// <returns>Command result: "SUCCESS" if succeeded</returns>
+	std::string send_start(art::RunID, uint64_t, uint64_t);
+
+	/// <summary>
+	/// Send a pause command over XMLRPC
+	/// 
+	/// The pause command pauses a Run. When the run resumes, the subrun number will be incremented.
+	/// This command accepts a timeout parameter and a timestamp parameter.
+	/// </summary>
+	/// <returns>Command result: "SUCCESS" if succeeded</returns>
+	std::string send_pause(uint64_t, uint64_t);
+
+	/// <summary>
+	/// Send a resume command over XMLRPC
+	/// 
+	/// The resume command resumes a paused Run. When the run resumes, the subrun number will be incremented.
+	/// This command accepts a timeout parameter and a timestamp parameter.
+	/// </summary>
+	/// <returns>Command result: "SUCCESS" if succeeded</returns>
+	std::string send_resume(uint64_t, uint64_t);
+
+	/// <summary>
+	/// Send a stop command over XMLRPC
+	/// 
+	/// The stop command stops the current Run.
+	/// This command accepts a timeout parameter and a timestamp parameter.
+	/// </summary>
+	/// <returns>Command result: "SUCCESS" if succeeded</returns>
+	std::string send_stop(uint64_t, uint64_t);
+
+	/// <summary>
+	/// Send a shutdown command over XMLRPC
+	/// 
+	/// The shutdown command shuts down the artdaq process.
+	/// This command accepts a timeout parameter.
+	/// </summary>
+	/// <returns>Command result: "SUCCESS" if succeeded</returns>
+	std::string send_shutdown(uint64_t);
+
+	/// <summary>
+	/// Send a status command over XMLRPC
+	/// 
+	/// The status command returns the current status of the artdaq process.
+	/// </summary>
+	/// <returns>Command result: current status of the artdaq process</returns>
+	std::string send_status();
+
+	/// <summary>
+	/// Send a report command over XMLRPC
+	/// 
+	/// The report command returns the current value of the requested reportable quantity.
+	/// </summary>
+	/// <returns>Command result: current value of the requested reportable quantity</returns>
+	std::string send_report(std::string);
+
+	/// <summary>
+	/// Send a legal_commands command over XMLRPC
+	/// 
+	/// This will query the artdaq process, and it will return the list of allowed transition commands from its current state.
+	/// </summary>
+	/// <returns>Command result: a list of allowed transition commands from its current state</returns>
+	std::string send_legal_commands();
+	
+	/// <summary>
+	/// Send an send_trace_get command over XMLRPC
+	/// 
+	/// This will cause the receiver to get the TRACE level masks for the given name
+	/// Use name == "ALL" to get ALL names
+	/// </summary>
+	/// <returns>Command result: "SUCCESS" if succeeded</returns>
+	std::string send_trace_get(std::string);
+
+	/// <summary>
+	/// Send an send_trace_msgfacility_set command over XMLRPC
+	/// 
+	/// This will cause the receiver to set the given TRACE level mask for the given name to the given mask.
+	/// Only the first character of the mask selection will be parsed, dial 'M' for Memory, or 'S' for Slow.
+	/// Use name == "ALL" to set ALL names
+	///
+	/// EXAMPLE: xmlrpc http://localhost:5235/RPC2 daq.trace_msgfacility_set TraceLock i/$((0x1234)) # Use Bash to convert hex to dec
+	/// </summary>
+	/// <returns>Command result: "SUCCESS" if succeeded</returns>
+	std::string send_trace_set(std::string, std::string, uint64_t);
+
+	/// <summary>
+	/// Send an send_meta_command command over XMLRPC
+	/// 
+	/// This will cause the receiver to run the given command with the given argument in user code
+	/// </summary>
+	/// <returns>Command result: "SUCCESS" if succeeded</returns>
+	std::string send_meta_command(std::string, std::string);
+
+	/// <summary>
+	/// Send a send_rollover_subrun command over XMLRPC
+	///
+	/// This will cause the receiver to rollover the subrun number at the given event. (Event with seqID == boundary will be in new subrun.)
+	/// Should be sent to all EventBuilders before the given event is processed.
+	/// </summary>
+	/// <returns>Command result: "SUCCESS" if succeeded</returns>
+	std::string send_rollover_subrun(uint64_t);
+
 
 private:
 	xmlrpc_commander(const xmlrpc_commander&) = delete;
@@ -56,6 +191,15 @@ private:
 
 	int port_;
 	std::string serverUrl_;
+
+	std::string send_command_(std::string command);
+	std::string send_command_(std::string command, std::string arg);
+	std::string send_command_(std::string command, fhicl::ParameterSet pset, uint64_t a, uint64_t b);
+	std::string send_command_(std::string command, uint64_t a, uint64_t b);
+	std::string send_command_(std::string command, art::RunID r, uint64_t a, uint64_t b);
+	std::string send_command_(std::string, uint64_t);
+	std::string send_command_(std::string, std::string, std::string);
+	std::string send_command_(std::string, std::string, std::string, uint64_t);
 
 public:
 	std::timed_mutex mutex_; ///< XMLRPC mutex
