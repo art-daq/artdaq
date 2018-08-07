d7e2263e9f8d1b77ebf7bc2d2cbe4ca6001f4f9b
Eric Flumerfelt <eflumerf@fnal.gov>
2018-08-07 12:30:44 -0500

    Add option "use_all_tokens" (defaults to false), which, when true, will keep round-robining between remaining receivers until all tokens are assigned. Compromise state between NoOp and full RoundRobin.

--- artdaq/Application/Routing/RoundRobin_policy.cc
+++ artdaq/Application/Routing/RoundRobin_policy.cc
@@ -15,9 +15,13 @@ namespace artdaq
 		 * \brief RoundRobinPolicy Constructor
 		 * \param ps ParameterSet used to configure RoundRobinPolicy
 		 * 
-		 * RoundRobinPolicy accepts no Parameters at this time.
+		 * RoundRobinPolicy accepts the following Parameter:
+		 * "use_all_tokens" (Default: false): Whether to use all tokens or to stop when we can no longer complete a "turn"
 		 */
-		explicit RoundRobinPolicy(fhicl::ParameterSet ps) : RoutingMasterPolicy(ps) {}
+		explicit RoundRobinPolicy(fhicl::ParameterSet ps)
+			: RoutingMasterPolicy(ps)
+			, use_all_tokens_(ps.get<bool>("use_all_tokens", false))
+		{}
 
 		/**
 		 * \brief Default virtual Destructor
@@ -33,6 +37,9 @@ namespace artdaq
 		 * "turns" through the recevier list.
 		 */
 		detail::RoutingPacket GetCurrentTable() override;
+
+	private:
+		bool use_all_tokens_;
 	};
 
 	detail::RoutingPacket RoundRobinPolicy::GetCurrentTable()
@@ -46,15 +53,19 @@ namespace artdaq
 		tokens->clear();
 
 		detail::RoutingPacket output;
-		auto endCondition = table.size() < GetReceiverCount();
+		auto endCondition = table.size() < use_all_tokens_ ? 1 : GetReceiverCount();
+
 		while (!endCondition)
 		{
-			for (auto r : table)
+			for (auto it = table.begin(); it != table.end();)
 			{
-				output.emplace_back(detail::RoutingPacketEntry(next_sequence_id_++, r.first));
-				if(!endCondition) endCondition = r.second == 1;
-				table[r.first]--;
+				output.emplace_back(detail::RoutingPacketEntry(next_sequence_id_++, it->first));
+				table[it->first]--;
+
+				if (table[it->first] <= 0) it = table.erase(it);
+				else ++it;
 			}
+			endCondition = table.size() < use_all_tokens_ ? 1 : GetReceiverCount();
 		}
 
 		for(auto r : table)
