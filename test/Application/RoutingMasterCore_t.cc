37bd12864643cbb823b5694e847ba1501108058a
Eric Flumerfelt <eflumerf@fnal.gov>
2019-04-04 08:39:42 -0500

    Add ability to receive table updates and send acks to RoutingMasterCoreTest class

--- test/Application/RoutingMasterCore_t.cc
+++ test/Application/RoutingMasterCore_t.cc
@@ -4,7 +4,9 @@
 #include <boost/test/auto_unit_test.hpp>
 
 #include "artdaq/Application/RoutingMasterCore.hh"
+#include "artdaq/DAQdata/TCPConnect.hh"
 #include "artdaq/DAQrate/RequestSender.hh"
+#include "sys/poll.h"
 
 /**
  * \brief RoutingMasterCoreTest class for testing (friend of RoutingMasterCore to collect internal state
@@ -62,8 +64,163 @@ public:
 	int const& get_table_socket_() { return routing_master_->table_socket_; }
 	int const& get_ack_socket_() { return routing_master_->ack_socket_; }
 
+	static fhicl::ParameterSet MakeRoutingMasterPset()
+	{
+		fhicl::ParameterSet ps;
+		fhicl::ParameterSet daq_ps;
+		daq_ps.put<std::vector<int>>("sender_ranks", {5, 6, 7});
+
+		fhicl::ParameterSet policy_ps;
+		policy_ps.put<std::vector<int>>("receiver_ranks", {1, 2, 3, 4});
+		policy_ps.put<std::string>("policy", "NoOp");
+		daq_ps.put<fhicl::ParameterSet>("policy", policy_ps);
+		ps.put<fhicl::ParameterSet>("daq", daq_ps);
+
+		return ps;
+	}
+
+	void setupTableListener()
+	{
+		int sts;
+		int table_socket_ = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+		if (table_socket_ < 0)
+		{
+			TLOG(TLVL_ERROR) << "Error creating socket for receiving table updates!";
+			exit(1);
+		}
+
+		struct sockaddr_in si_me_request;
+
+		int yes = 1;
+		if (setsockopt(table_socket_, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) < 0)
+		{
+			TLOG(TLVL_ERROR) << " Unable to enable port reuse on request socket";
+			exit(1);
+		}
+		memset(&si_me_request, 0, sizeof(si_me_request));
+		si_me_request.sin_family = AF_INET;
+		si_me_request.sin_port = htons(get_send_tables_port_());
+		//si_me_request.sin_addr.s_addr = htonl(INADDR_ANY);
+		struct in_addr in_addr_s;
+		sts = inet_aton(get_send_tables_address_().c_str(), &in_addr_s);
+		if (sts == 0)
+		{
+			TLOG(TLVL_ERROR) << "inet_aton says table_address " << get_send_tables_address_() << " is invalid";
+		}
+		si_me_request.sin_addr.s_addr = in_addr_s.s_addr;
+		if (bind(table_socket_, (struct sockaddr*)&si_me_request, sizeof(si_me_request)) == -1)
+		{
+			TLOG(TLVL_ERROR) << "Cannot bind request socket to port " << get_send_tables_port_();
+			exit(1);
+		}
+
+		struct ip_mreq mreq;
+		sts = ResolveHost(get_send_tables_address_().c_str(), mreq.imr_multiaddr);
+		if (sts == -1)
+		{
+			TLOG(TLVL_ERROR) << "Unable to resolve multicast address for table updates";
+			exit(1);
+		}
+		mreq.imr_interface.s_addr = htonl(INADDR_ANY);
+		if (setsockopt(table_socket_, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq)) < 0)
+		{
+			TLOG(TLVL_ERROR) << "Unable to join multicast group";
+			exit(1);
+		}
+	}
+
+	artdaq::detail::RoutingPacket receiveTableUpdate()
+	{
+		TLOG(TLVL_TRACE) << __func__ << ": Polling table socket for new routes";
+		if (table_socket_ == -1)
+		{
+			TLOG(TLVL_DEBUG) << __func__ << ": Opening table listener socket";
+			setupTableListener();
+		}
+		if (table_socket_ == -1)
+		{
+			TLOG(TLVL_DEBUG) << __func__ << ": The listen socket was not opened successfully.";
+			return artdaq::detail::RoutingPacket(0);
+		}
+
+		struct pollfd fd;
+		fd.fd = table_socket_;
+		fd.events = POLLIN | POLLPRI;
+
+		auto res = poll(&fd, 1, 10000);
+		if (res > 0)
+		{
+			std::vector<uint8_t> buf(MAX_ROUTING_TABLE_SIZE);
+			artdaq::detail::RoutingPacketHeader hdr;
+
+			TLOG(TLVL_DEBUG) << __func__ << ": Going to receive RoutingPacketHeader";
+			struct sockaddr_in from;
+			socklen_t len = sizeof(from);
+			auto stss = recvfrom(table_socket_, &buf[0], MAX_ROUTING_TABLE_SIZE, 0, (struct sockaddr*)&from, &len);
+			TLOG(TLVL_DEBUG) << __func__ << ": Received " << stss << " bytes from " << inet_ntoa(from.sin_addr) << ":" << from.sin_port;
+
+			if (stss > static_cast<ssize_t>(sizeof(hdr)))
+			{
+				memcpy(&hdr, &buf[0], sizeof(artdaq::detail::RoutingPacketHeader));
+			}
+			else
+			{
+				TLOG(TLVL_TRACE) << __func__ << ": Incorrect size received. Discarding.";
+				return artdaq::detail::RoutingPacket(0);
+			}
+
+			TRACE(TLVL_DEBUG, "receiveTableUpdatesLoop_: Checking for valid header with nEntries=%lu headerData:0x%016lx%016lx", hdr.nEntries, ((unsigned long*)&hdr)[0], ((unsigned long*)&hdr)[1]);
+			if (hdr.header != ROUTING_MAGIC)
+			{
+				TLOG(TLVL_TRACE) << __func__ << ": non-RoutingPacket received. No ROUTING_MAGIC. size(bytes)=" << stss;
+				return artdaq::detail::RoutingPacket(0);
+			}
+			if (get_routing_mode_() != artdaq::detail::RoutingMasterMode::INVALID && get_routing_mode_() != hdr.mode)
+			{
+				TLOG(TLVL_ERROR) << __func__ << ": Received table has different RoutingMasterMode than expected!";
+				return artdaq::detail::RoutingPacket(0);
+			}
+
+			artdaq::detail::RoutingPacket buffer(hdr.nEntries);
+			assert(static_cast<size_t>(stss) == sizeof(artdaq::detail::RoutingPacketHeader) + sizeof(artdaq::detail::RoutingPacketEntry) * hdr.nEntries);
+			memcpy(&buffer[0], &buf[sizeof(artdaq::detail::RoutingPacketHeader)], sizeof(artdaq::detail::RoutingPacketEntry) * hdr.nEntries);
+			TRACE(6, "receiveTableUpdatesLoop_: Received a packet of %ld bytes. 1st 16 bytes: 0x%016lx%016lx", stss, ((unsigned long*)&buffer[0])[0], ((unsigned long*)&buffer[0])[1]);
+
+			return buffer;
+		}
+		TLOG(TLVL_ERROR) << "Error receiving routing table from Routing master: " << errno;
+		return artdaq::detail::RoutingPacket(0);
+	}
+
+	void sendAck(int rank, uint64_t first, uint64_t last)
+	{
+		if (ack_socket_ == -1)
+		{
+			ack_socket_ = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+			auto sts = ResolveHost(get_receive_address_().c_str(), get_receive_acks_port_(), ack_addr_);
+			if (sts == -1)
+			{
+				TLOG(TLVL_ERROR) << __func__ << ": Unable to resolve routing_master_address";
+				exit(1);
+			}
+			TLOG(TLVL_DEBUG) << __func__ << ": Ack socket is fd " << ack_socket_;
+		}
+
+		artdaq::detail::RoutingAckPacket ack;
+		ack.rank = rank;
+		ack.first_sequence_id = first;
+		ack.last_sequence_id = last;
+		
+		TLOG(TLVL_DEBUG) << __func__ << ": Sending RoutingAckPacket with first= " << first << " and last= " << last << " to " << get_receive_address_() << ", port " << get_receive_acks_port_() << " (rank = " << rank << ")";
+		sendto(ack_socket_, &ack, sizeof(artdaq::detail::RoutingAckPacket), 0, (struct sockaddr*)&ack_addr_, sizeof(ack_addr_));
+	}
+
 private:
 	artdaq::RoutingMasterCore* routing_master_;
+
+	int table_socket_;
+	int ack_socket_;
+	struct sockaddr_in ack_addr_;
 };
 
 BOOST_AUTO_TEST_SUITE(RoutingMasterCore_t)
@@ -82,15 +239,7 @@ BOOST_AUTO_TEST_CASE(Construct)
 BOOST_AUTO_TEST_CASE(Initialize)
 {
 	TLOG(TLVL_INFO) << "Test case Initialize START";
-	fhicl::ParameterSet ps;
-	fhicl::ParameterSet daq_ps;
-	daq_ps.put<std::vector<int>>("sender_ranks", {5, 6, 7});
-
-	fhicl::ParameterSet policy_ps;
-	policy_ps.put<std::vector<int>>("receiver_ranks", {1, 2, 3, 4});
-	policy_ps.put<std::string>("policy", "NoOp");
-	daq_ps.put<fhicl::ParameterSet>("policy", policy_ps);
-	ps.put<fhicl::ParameterSet>("daq", daq_ps);
+	fhicl::ParameterSet ps = artdaqtest::RoutingMasterCoreTest::MakeRoutingMasterPset();
 
 	artdaq::RoutingMasterCore core;
 	artdaqtest::RoutingMasterCoreTest coreTest(core);
@@ -111,15 +260,7 @@ BOOST_AUTO_TEST_CASE(Initialize)
 BOOST_AUTO_TEST_CASE(StateMachine)
 {
 	TLOG(TLVL_INFO) << "Test case StateMachine START";
-	fhicl::ParameterSet ps;
-	fhicl::ParameterSet daq_ps;
-	daq_ps.put<std::vector<int>>("sender_ranks", {5, 6, 7});
-
-	fhicl::ParameterSet policy_ps;
-	policy_ps.put<std::vector<int>>("receiver_ranks", {1, 2, 3, 4});
-	policy_ps.put<std::string>("policy", "NoOp");
-	daq_ps.put<fhicl::ParameterSet>("policy", policy_ps);
-	ps.put<fhicl::ParameterSet>("daq", daq_ps);
+	fhicl::ParameterSet ps = artdaqtest::RoutingMasterCoreTest::MakeRoutingMasterPset();
 
 	// All of these transitions should work without exceptions
 	std::unique_ptr<artdaq::RoutingMasterCore> coreptr(new artdaq::RoutingMasterCore());
@@ -181,15 +322,7 @@ BOOST_AUTO_TEST_CASE(StateMachine)
 BOOST_AUTO_TEST_CASE(SendTokens)
 {
 	TLOG(TLVL_INFO) << "Test case SendTokens START";
-	fhicl::ParameterSet ps;
-	fhicl::ParameterSet daq_ps;
-	daq_ps.put<std::vector<int>>("sender_ranks", {5, 6, 7});
-
-	fhicl::ParameterSet policy_ps;
-	policy_ps.put<std::vector<int>>("receiver_ranks", {1, 2, 3, 4});
-	policy_ps.put<std::string>("policy", "NoOp");
-	daq_ps.put<fhicl::ParameterSet>("policy", policy_ps);
-	ps.put<fhicl::ParameterSet>("daq", daq_ps);
+	fhicl::ParameterSet ps = artdaqtest::RoutingMasterCoreTest::MakeRoutingMasterPset();
 
 	artdaq::RoutingMasterCore core;
 	artdaqtest::RoutingMasterCoreTest coreTest(core);
@@ -207,7 +340,7 @@ BOOST_AUTO_TEST_CASE(SendTokens)
 	my_rank = 3;
 	// This token will be held until after the start of the run
 	rs.SendRoutingToken(1, 1);
-
+	usleep(100000);
 	BOOST_REQUIRE_EQUAL(coreTest.get_received_token_count_(), 0);
 
 	core.start(art::RunID(1), 0ULL, 0ULL);
@@ -216,22 +349,54 @@ BOOST_AUTO_TEST_CASE(SendTokens)
 
 	rs.SendRoutingToken(1, 1);
 	usleep(100000);
-
 	BOOST_REQUIRE_EQUAL(coreTest.get_received_token_count_(), 2);
 
 	//This token should be ignored, as it is for the wrong run
 	rs.SendRoutingToken(1, 5);
 	usleep(100000);
-
 	BOOST_REQUIRE_EQUAL(coreTest.get_received_token_count_(), 2);
 
 	// This token should be ignored, as it is not from a registered receiver
 	my_rank = 10;
 	rs.SendRoutingToken(1, 1);
 	usleep(100000);
-
 	BOOST_REQUIRE_EQUAL(coreTest.get_received_token_count_(), 2);
+
+	my_rank = 8;
+	rs.SendRoutingToken(1, 1);
+	usleep(100000);
+	BOOST_REQUIRE_EQUAL(coreTest.get_received_token_count_(), 3);
+
+	rs.SendRoutingToken(0, 1);
+	usleep(100000);
+	BOOST_REQUIRE_EQUAL(coreTest.get_received_token_count_(), 3);
+
+	rs.SendRoutingToken(10, 1);
+	usleep(100000);
+	BOOST_REQUIRE_EQUAL(coreTest.get_received_token_count_(), 13);
+
 	TLOG(TLVL_INFO) << "Test case SendTokens END";
+}
+
+BOOST_AUTO_TEST_CASE(Tables)
+{
+	TLOG(TLVL_INFO) << "Test case Tables BEGIN";
+	fhicl::ParameterSet ps = artdaqtest::RoutingMasterCoreTest::MakeRoutingMasterPset();
+
+	artdaq::RoutingMasterCore core;
+	artdaqtest::RoutingMasterCoreTest coreTest(core);
+
+	auto sts = core.initialize(ps, 0ULL, 0ULL);
+	BOOST_REQUIRE_EQUAL(sts, true);
+
+	fhicl::ParameterSet sender_ps;
+	fhicl::ParameterSet sender_rm_ps;
+	sender_rm_ps.put<bool>("use_routing_master", true);
+	sender_ps.put<fhicl::ParameterSet>("routing_token_config", sender_rm_ps);
+
+	artdaq::RequestSender rs(sender_ps);
+
+	TLOG(TLVL_INFO) << "Test case Tables END";
 }
 
 BOOST_AUTO_TEST_SUITE_END()
