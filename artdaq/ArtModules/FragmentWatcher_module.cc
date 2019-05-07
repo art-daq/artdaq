edf624e9ead49c1d4133818efb4418c06cad9646
Kurt Biery <biery@fnal.gov>
2019-05-07 17:35:34 -0500

    Added rudimentary reporting of Empty fragments by fragmentID to FragmentWatcher_module.

--- artdaq/ArtModules/FragmentWatcher_module.cc
+++ artdaq/ArtModules/FragmentWatcher_module.cc
@@ -5,6 +5,9 @@
 // Description: Collects and reports statistics on missing and empty fragments
 ////////////////////////////////////////////////////////////////////////
 
+#define TRACE_NAME (app_name + "_FragmentWatcher").c_str()
+#include "artdaq/DAQdata/Globals.hh"
+
 #include "art/Framework/Core/EDAnalyzer.h"
 #include "art/Framework/Core/ModuleMacros.h"
 #include "art/Framework/Principal/Event.h"
@@ -12,14 +15,10 @@
 
 #include "artdaq-core/Data/Fragment.hh"
 #include "artdaq-core/Data/ContainerFragment.hh"
-#include "artdaq/DAQdata/Globals.hh"
-
-#include "messagefacility/MessageLogger/MessageLogger.h"
 
 #include <iostream>
 #include <bitset>
-
-#define TRACE_NAME (app_name + "_FragmentWatcher").c_str()
+#include <map>
 
 namespace artdaq
 {
@@ -49,6 +48,8 @@ private:
   int events_with_50pct_missing_fragments_;
   int events_with_50pct_empty_fragments_;
 
+  std::map<int, int> empty_fragments_by_fragmentID_;
+
   const int BASIC_COUNTS_MODE = 0;
   const int FRACTIONAL_COUNTS_MODE = 1;
   const int DETAILED_COUNTS_MODE = 2;
@@ -61,7 +62,8 @@ artdaq::FragmentWatcher::FragmentWatcher(fhicl::ParameterSet const & pset)
     events_processed_(0), expected_number_of_fragments_(0),
     events_with_missing_fragments_(0), events_with_empty_fragments_(0),
     events_with_10pct_missing_fragments_(0), events_with_10pct_empty_fragments_(0),
-    events_with_50pct_missing_fragments_(0), events_with_50pct_empty_fragments_(0)
+    events_with_50pct_missing_fragments_(0), events_with_50pct_empty_fragments_(0),
+    empty_fragments_by_fragmentID_()
 {
   fhicl::ParameterSet metric_pset;
   try
@@ -98,34 +100,63 @@ void artdaq::FragmentWatcher::analyze(art::Event const & evt)
   evt.getManyByType(fragmentHandles);
 
   // count total fragments
-  int total_fragments = 0;
+  int total_fragments_this_event = 0;
   for (auto const& hndl : fragmentHandles)
   {
-    total_fragments += hndl->size();
+    total_fragments_this_event += hndl->size();
   }
 
   // update the expected number of fragments, if needed
-  if (total_fragments > expected_number_of_fragments_) {expected_number_of_fragments_ = total_fragments;}
+  if (total_fragments_this_event > expected_number_of_fragments_) {expected_number_of_fragments_ = total_fragments_this_event;}
 
   // check if this event has fewer fragments than expected
-  int missing_fragments = expected_number_of_fragments_ - total_fragments;
+  int missing_fragments = expected_number_of_fragments_ - total_fragments_this_event;
 
   // check if this event has any Empty fragments
-  int empty_fragments = 0; 
+  int empty_fragment_count_this_event = 0;
+  std::set<int> empty_fragmentID_list_this_event;
   for (auto const& hndl : fragmentHandles)
   {
     std::string instance_name = hndl.provenance()->productInstanceName();
     std::size_t found = instance_name.find("Empty");
     if (found != std::string::npos)
     {
-      empty_fragments += hndl->size();
+      empty_fragment_count_this_event += hndl->size();
+
+      // track the number of empty fragments by fragment ID
+      for (auto const& fragment : *hndl)
+      {
+        int fragID = fragment.fragmentID();
+        if (empty_fragments_by_fragmentID_.count(fragID) == 0)
+        {
+          empty_fragments_by_fragmentID_[fragID] = 1;
+        }
+        else
+        {
+          empty_fragments_by_fragmentID_[fragID] += 1;
+        }
+        empty_fragmentID_list_this_event.insert(fragID);
+      }
     }
   }
 
   // provide diagnostic TRACE message(s) about this event
-  TLOG(TLVL_TRACE) << "Event " << evt.event() << ": total_fragments=" << total_fragments << ", missing_fragments="
-                   << missing_fragments << ", empty_fragments=" << empty_fragments << " (" << events_processed_
+  TLOG(TLVL_TRACE) << "Event " << evt.event() << ": total_fragments=" << total_fragments_this_event << ", missing_fragments="
+                   << missing_fragments << ", empty_fragments=" << empty_fragment_count_this_event << " (" << events_processed_
                    << " events processed)";
+  if (empty_fragmentID_list_this_event.size() > 0)
+  {
+    std::ostringstream oss;
+    bool firstLoop = true;
+    for (auto const& fragID : empty_fragmentID_list_this_event)
+    {
+      if (! firstLoop) {oss << ", ";}
+      oss << fragID;
+      firstLoop = false;
+    }
+    TLOG(TLVL_WARNING) << "Event " << evt.event() << ": total_fragments=" << total_fragments_this_event
+                       << ", fragmentIDs for empty_fragments: " << oss.str();
+  }
 
   // common metric reporting for multiple modes
   if (metricMan != nullptr && (mode_bitset_.test(BASIC_COUNTS_MODE) || mode_bitset_.test(FRACTIONAL_COUNTS_MODE)))
@@ -138,7 +169,7 @@ void artdaq::FragmentWatcher::analyze(art::Event const & evt)
   if (metricMan != nullptr && mode_bitset_.test(BASIC_COUNTS_MODE))
   {
     if (missing_fragments > 0) {++events_with_missing_fragments_;}
-    if (empty_fragments > 0) {++events_with_empty_fragments_;}
+    if (empty_fragment_count_this_event > 0) {++events_with_empty_fragments_;}
 
     metricMan->sendMetric("EventsWithMissingFragments", events_with_missing_fragments_, "events",
                           metrics_reporting_level_, artdaq::MetricMode::LastPoint);
@@ -161,11 +192,11 @@ void artdaq::FragmentWatcher::analyze(art::Event const & evt)
       ++events_with_50pct_missing_fragments_;
     }
 
-    if (((static_cast<double>(empty_fragments) * 100.0) / static_cast<double>(expected_number_of_fragments_)) >= 10.0)
+    if (((static_cast<double>(empty_fragment_count_this_event) * 100.0) / static_cast<double>(expected_number_of_fragments_)) >= 10.0)
     {
       ++events_with_10pct_empty_fragments_;
     }
-    if (((static_cast<double>(empty_fragments) * 100.0) / static_cast<double>(expected_number_of_fragments_)) >= 50.0)
+    if (((static_cast<double>(empty_fragment_count_this_event) * 100.0) / static_cast<double>(expected_number_of_fragments_)) >= 50.0)
     {
       ++events_with_50pct_empty_fragments_;
     }
@@ -186,7 +217,26 @@ void artdaq::FragmentWatcher::analyze(art::Event const & evt)
                      << ", events_with_50pct_empty_fragments=" << events_with_50pct_empty_fragments_;
   }
 
-  // still to be done: implement the 'detailed_counts' mode
+  // initial reporting for the DETAILED_COUNTS_MODE
+  if (metricMan != nullptr && mode_bitset_.test(DETAILED_COUNTS_MODE))
+  {
+    // only send an update when the empty fragment count, by FragmentID, changed,
+    // as indicated by a non-zero number of empty fragments in this event
+    if (empty_fragment_count_this_event > 0)
+    {
+      std::ostringstream oss;
+      oss << "<eventbuilder_snapshot app_name=\"" << app_name << "\"><events_processed>" << events_processed_
+          << "</events_processed><empty_fragment_counts>";
+      for (auto const& mapIter : empty_fragments_by_fragmentID_)
+      {
+        oss << "<count fragment_id=" << mapIter.first << ">" << mapIter.second << "</count>";
+      }
+      oss << "</empty_fragment_counts></eventbuilder_snapshot>";
+
+      metricMan->sendMetric("EmptyFragmentSnapshot", oss.str(), "xml_string",
+                            metrics_reporting_level_, artdaq::MetricMode::LastPoint);
+    }
+  }
 
 #if 0
 =====================================================
