983a8a5409e1758257b1a6c13ace25f949756d47
Eric Flumerfelt <eflumerf@fnal.gov>
2019-11-22 13:59:01 -0600

    Run clang-format on all source files

--- artdaq/ArtModules/BuildInfo_module.hh
+++ artdaq/ArtModules/BuildInfo_module.hh
@@ -108,7 +108,7 @@ void BuildInfo<instanceName, Pkgs...>::beginRun(art::Run& e)
 	auto packages_deep_copy_ptr = std::unique_ptr<std::vector<PackageBuildInfo>>(
 	    new std::vector<PackageBuildInfo>(*packages_));
 
-	TLOG(TLVL_INFO, "BuildInfo") << "Placing BuildInfo with instance name \"" << instanceName_ << "\"" ;
+	TLOG(TLVL_INFO, "BuildInfo") << "Placing BuildInfo with instance name \"" << instanceName_ << "\"";
 	e.put(std::move(packages_deep_copy_ptr), instanceName_);
 }
 
