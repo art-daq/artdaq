9568126c1c775a3f51d93d96ae2207eaaa2e745e
Eric Flumerfelt <eflumerf@fnal.gov>
2021-07-09 15:19:13 -0500

    Remove unused paramters

--- artdaq/DAQrate/detail/TableReceiver.hh
+++ artdaq/DAQrate/detail/TableReceiver.hh
@@ -132,7 +132,6 @@ private:
 	mutable std::condition_variable routing_cv_;
 
 	size_t routing_timeout_ms_;
-	int routing_retry_count_;
 
 	mutable std::atomic<uint64_t> highest_sequence_id_routed_;
 };
