7265a085bb9826e5977c9a8c41886bb14bb22f9b
Eric Flumerfelt <eflumerf@fnal.gov>
2021-09-03 19:04:00 -0500

    Add missing include

--- artdaq/ArtModules/RequestSender_module.cc
+++ artdaq/ArtModules/RequestSender_module.cc
@@ -11,6 +11,7 @@
 #include "art/Framework/Core/EDAnalyzer.h"
 #include "art/Framework/Core/ModuleMacros.h"
 #include "art/Framework/Principal/Event.h"
+#include "art/Framework/Principal/Run.h"
 #include "art/Framework/Principal/Handle.h"
 
 #include "artdaq-core/Data/ContainerFragment.hh"
