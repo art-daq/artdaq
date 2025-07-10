d853a7ba66b2d06f4758d754369de2ebdfa26c0e
Eric Flumerfelt <eflumerf@fnal.gov>
2025-07-10 15:27:06 -0500

    Add version of TCPSocket_transfer which waits for ack messages

---
+++ artdaq/TransferPlugins/TCPSocketWithAcks_transfer.cc
@@ -0,0 +1,11 @@
+// Sep 14, 2016. "TERMS AND CONDITIONS" governing this file are in the README
+// or COPYING file. If you do not have such a file, one can be obtained by
+// contacting Ron or Fermi Lab in Batavia IL, 60510, phone: 630-840-3000.
+// $RCSfile: .emacs.gnu,v $
+// rev="$Revision: 1.30 $$Date: 2016/03/01 14:27:27 $";
+
+#define USE_ACKS 1
+
+#include "artdaq/TransferPlugins/TCPSocketTransfer.hh"
+
+DEFINE_ARTDAQ_TRANSFER(artdaq::TCPSocketTransfer)
