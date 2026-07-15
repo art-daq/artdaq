8507830fb4775f27e5a65b009592355f3d87a987
Eric Flumerfelt <eflumerf@fnal.gov>
2026-07-15 07:51:56 -0500

    Limit list size

--- artdaq/ArtModules/ArtdaqSharedMemoryService_service.cc
+++ artdaq/ArtModules/ArtdaqSharedMemoryService_service.cc
@@ -315,6 +315,7 @@ std::shared_ptr<ArtdaqEvent> ArtdaqSharedMemoryService::ReceiveEvent(bool broadc
 					{
 						output_event = event_ordering_.front();
 						released_broadcast_sequence_ids_.insert(event_ordering_.front()->header->sequence_id);
+						while (released_broadcast_sequence_ids_.size() > 1000) { released_broadcast_sequence_ids_.erase(released_broadcast_sequence_ids_.begin()); }
 					}
 					event_ordering_.pop_front();
 					continue;  // while(output_event == nullptr)
