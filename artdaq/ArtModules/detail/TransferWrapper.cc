23e3518d581e7f96967d0d60b9c96f799ab17f0d
Simon Corrodi <scorrodi@anl.gov>
2026-07-18 16:49:19 -0500

    clangify

--- artdaq/ArtModules/detail/TransferWrapper.cc
+++ artdaq/ArtModules/detail/TransferWrapper.cc
@@ -147,8 +147,8 @@ artdaq::FragmentPtrs artdaq::TransferWrapper::receiveMessage()
 						suffix = "-rd";
 					}
 					TLOG(TLVL_DEBUG + 32) << "Received " << cntr << suffix << " event, "
-					                << "seqID == " << fragmentPtr->sequenceID()
-					                << ", type == " << fragmentPtr->typeString();
+					                      << "seqID == " << fragmentPtr->sequenceID()
+					                      << ", type == " << fragmentPtr->typeString();
 					last_received_data_ = std::chrono::steady_clock::now();
 					continue;
 				}
